4b54af8e680d5ffcd5ffbcd4c8b42947e02a6ab6
pallav <pallav.dev@outlook.com>
2024-01-02 19:03:54 +0530

    Update index.js

--- Rest-API/index.js
+++ Rest-API/index.js
@@ -4,13 +4,13 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 require("./db/connection")
-const products_routes = require("./routes/attendance")
+const routes = require("./routes/attendance")
 
 app.get("/",(req,res)=>{
     res.send("Hi, I am live");
 })
 
-app.use("/api/attendance",products_routes);
+app.use("/api/attendance",routes);
 
 const start = async()=>{
     try {
